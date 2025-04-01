0f357378eb18bb980a6bcad875edf2d66f03fde8
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-04-01 13:35:35 +0530

    feat: initial setup completed for blog

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,5 @@
+'use server'
+export default async function PostPage(){
+    //read me from the page and then pass it on to here voila
+    return <p>hello from this page</p>
+}
---
+++ src/posts/first.md
@@ -0,0 +1,8 @@
+---
+title: 'My First Blog Post'
+date: '2025-04-01'
+---
+
+# Welcome to my blog!
+
+This is the content of my first blog post written in **Markdown**.
